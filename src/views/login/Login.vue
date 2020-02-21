3f5949a072db4cf929c94386c7d77f92b2d15550
franselles <fran.selles@gmail.com>
2020-02-21 12:13:24 +0100

    V-0-1-23

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -17,7 +17,7 @@
                   v-model="pin"
                 />
                 <button type="submit">Acceder</button>
-                <a href="#">0.1.22</a>
+                <a href="#">0.1.23</a>
               </form>
             </div>
           </div>
