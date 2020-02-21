10023538b7f8a514f72e3f31df7d7c2cffdc0fe9
franselles <fran.selles@gmail.com>
2020-02-21 12:18:56 +0100

    V-0-1-24

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -22,6 +22,7 @@ if (process.env.NODE_ENV === 'production') {
     updated() {
       console.log('New content is available; please refresh.');
       alert('New content is available; please refresh.');
+      window.location.reload(true);
     },
     offline() {
       console.log(
