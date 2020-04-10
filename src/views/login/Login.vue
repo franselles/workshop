b8774a408fd0776e7494961957149d08e8429824
franselles <fran.selles@gmail.com>
2020-04-10 11:15:30 +0200

    Fixed packages and vars

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -29,7 +29,7 @@
                   </button>
                 </div>
                 <div class="field">
-                  <label for="">0.1.40</label>
+                  <label for="">0.1.41</label>
                 </div>
               </form>
             </div>
